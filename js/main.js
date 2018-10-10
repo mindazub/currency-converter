e8e845daad4b8bce1c8d0e37aae0f7dfdd1959cc
Mindaugas Azubalis <mindaugas.azubalis@gmail.com>
2018-10-10 17:06:36 +0300

    all done.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,36 @@
+$("#amount_from").on('keyup', function () {
+    convert();
+});
+
+$("#currency_from").on('change', function () {
+    convert();
+});
+
+$("#currency_to").on('change', function () {
+    convert();
+});
+
+$('#swap').on('click', function (e) {
+    e.preventDefault();
+    let currency_from = $('#currency_from').val();
+    let currency_to = $('#currency_to').val();
+    $('#currency_from').val(currency_to);
+    $('#currency_to').val(currency_from);
+    convert();
+});
+
+convert();
+
+function convert() {
+    var currency_from = $('#currency_from').val();
+    var currency_to = $('#currency_to').val();
+    var amount_from = $('#amount_from').val();
+    if (currency_from === currency_to) {
+        $('#amount_to').val(amount_from);
+    } else {
+        getRates(currency_from, currency_to, amount_from);
+    }
+}
 
 function getRates(currency_from, currency_to, amount_from ) {
     $.ajax({
